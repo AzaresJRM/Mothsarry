be57a16e0d364b994795dc22d7aa4326b1b4520f
AzaresJRM <jrmontano@usa.edu.ph>
2024-10-21 00:23:24 +0800

    Add files via upload

---
+++ Monthsarry(Ai)/config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "Monthsarry",
+    introTitle: 'Happy Monthsarry Aii <3 <3 <3 ',
+    introDesc: `After all this time, my destiny was right beside me in the office, more like a soulmate. Every moment spent together feels like a beautiful chapter in our story. From the little laughs during breaks to the shared dreams for the future, I cherish every second with you, Ai. You make the ordinary feel extraordinary, and I can't wait to see where this journey takes us. I love you! Like Super duper hehehhe`,
+    btnIntro: '^^HiHi^^',
+    title: 'Is it true that you fell in love at first sight? 🥰',
+    desc: 'Is it true that you were enchanted the moment you saw that smile?',
+    btnYes: 'I like it a lot <33',
+    btnNo: 'No way :3',
+    question: 'With more than 7 billion people in the world, why do you love me? <3',
+    btnReply: 'Send it to you <3',
+    reply: 'Love is love , kung hindi moko lovee , you may now stop breathing HAHAHAHAH Joke lang po Iloveeyouuuuuuu',
+    mess: 'I knew it 🥰. Love you so much 😘😘',
+    messDesc: 'I will see you tomorrow lovee dulo date2 naman ta a . MwaaaMwaaa',
+    btnAccept: 'Chat moko di Hehheehhe <3',
+    messLink: 'https://web.facebook.com/russel.montano.988' 
+}
